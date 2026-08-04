8f69dc738f46cf1048e66d2e459acfd202547f13
mroczect <mroczect@proton.me>
2026-08-04 19:48:11 +0700

    feat(domain): add user info

---
+++ src/domain/user.rs
@@ -0,0 +1,13 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct UserInfo {
+    pub name: String,
+    pub email: String,
+}
+
+impl UserInfo {
+    pub fn new(name: String, email: String) -> Self {
+        Self { name, email }
+    }
+}
