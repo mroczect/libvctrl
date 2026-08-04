172765cfaefa0f586ff814b0e893c6c7c9512d32
mroczect <mroczect@proton.me>
2026-08-04 19:48:12 +0700

    feat(merge): add merge traits

--- src/merge/mod.rs
+++ src/merge/mod.rs
@@ -1,5 +1,24 @@
 pub mod resolver;
 pub mod three_way;
-
 pub use resolver::*;
 pub use three_way::*;
+
+use crate::codec::Encoder;
+use crate::domain::hash::Hash;
+use crate::error::VctrlError;
+use crate::hashing::Hasher;
+use crate::storage::traits::ObjectStore;
+
+#[allow(clippy::too_many_arguments)]
+pub trait ThreeWayMerge {
+    fn merge(
+        &self,
+        store: &mut dyn ObjectStore,
+        base: &Hash,
+        ours: &Hash,
+        theirs: &Hash,
+        resolver: &dyn ConflictResolver,
+        encoder: &dyn Encoder,
+        hasher: &dyn Hasher,
+    ) -> Result<Hash, VctrlError>;
+}
