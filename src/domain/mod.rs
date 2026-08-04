48019eaa02bf4a744163022cd15dad89f09eff0e
mroczect <mroczect@proton.me>
2026-08-04 19:33:01 +0700

    refactor(domain): add module with all domain re-exports

---
+++ src/domain/mod.rs
@@ -0,0 +1,13 @@
+pub mod blob;
+pub mod commit;
+pub mod hash;
+pub mod object;
+pub mod tree;
+pub mod user;
+
+pub use blob::*;
+pub use commit::*;
+pub use hash::*;
+pub use object::*;
+pub use tree::*;
+pub use user::*;
