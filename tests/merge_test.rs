2aa159fb8243b96665d939ab2591fb659a0661f4
mroczect <mroczect@proton.me>
2026-08-04 19:48:16 +0700

    test: update merge test

--- tests/merge_test.rs
+++ tests/merge_test.rs
@@ -1,90 +1,112 @@
 mod common;
-use common::setup_store;
+use common::{encoder, hasher, put_blob, put_tree, setup_refs, setup_store};
 
-use libvctrl::{Blob, EntryKind, Object, ObjectStore, Tree, TreeEntry, VctrlError, merge_trees};
+use libvctrl::{
+    Command, ConflictResolver, EntryKind, MergeCommand, Object, ObjectStore, ThreeWayMerger, Tree,
+    TreeEntry, VctrlError,
+};
+
+struct SimpleResolver;
+impl ConflictResolver for SimpleResolver {
+    fn resolve(&self, _base: &[u8], _ours: &[u8], _theirs: &[u8]) -> Option<Vec<u8>> {
+        None
+    }
+}
+
+struct KeepOursResolver;
+impl ConflictResolver for KeepOursResolver {
+    fn resolve(&self, _base: &[u8], ours: &[u8], _theirs: &[u8]) -> Option<Vec<u8>> {
+        Some(ours.to_vec())
+    }
+}
 
 #[test]
-fn merge_no_conflict_simple() {
+fn merge_no_conflict() {
     let mut store = setup_store();
+    let mut refs = setup_refs();
+    let a1 = put_blob(&mut store, b"1");
+    let b2 = put_blob(&mut store, b"2");
 
-    let a1 = store.put(&Object::Blob(Blob::new(b"1".to_vec()))).unwrap();
-    let b2 = store.put(&Object::Blob(Blob::new(b"2".to_vec()))).unwrap();
     let base_tree = Tree::new(vec![
         TreeEntry::new("a".into(), EntryKind::Blob, a1),
         TreeEntry::new("b".into(), EntryKind::Blob, b2),
     ])
     .unwrap();
-    let base_hash = store.put(&Object::Tree(base_tree)).unwrap();
+    let base_hash = put_tree(&mut store, &base_tree);
 
-    let b3 = store.put(&Object::Blob(Blob::new(b"3".to_vec()))).unwrap();
+    let b3 = put_blob(&mut store, b"3");
     let ours_tree = Tree::new(vec![
         TreeEntry::new("a".into(), EntryKind::Blob, a1),
         TreeEntry::new("b".into(), EntryKind::Blob, b3),
     ])
     .unwrap();
-    let ours_hash = store.put(&Object::Tree(ours_tree)).unwrap();
+    let ours_hash = put_tree(&mut store, &ours_tree);
 
-    let c4 = store.put(&Object::Blob(Blob::new(b"4".to_vec()))).unwrap();
+    let c4 = put_blob(&mut store, b"4");
     let theirs_tree = Tree::new(vec![
         TreeEntry::new("a".into(), EntryKind::Blob, a1),
         TreeEntry::new("b".into(), EntryKind::Blob, b2),
         TreeEntry::new("c".into(), EntryKind::Blob, c4),
     ])
     .unwrap();
-    let theirs_hash = store.put(&Object::Tree(theirs_tree)).unwrap();
+    let theirs_hash = put_tree(&mut store, &theirs_tree);
 
-    let resolver = |_: &[u8], _: &[u8], _: &[u8]| -> Option<Vec<u8>> { None };
-
-    let merged_hash =
-        merge_trees(&mut store, &base_hash, &ours_hash, &theirs_hash, &resolver).unwrap();
-    let merged_tree = match store.get(&merged_hash).unwrap() {
-        Some(Object::Tree(t)) => t,
+    let cmd = MergeCommand {
+        base: base_hash,
+        ours: ours_hash,
+        theirs: theirs_hash,
+        merger: Box::new(ThreeWayMerger),
+        resolver: Box::new(SimpleResolver),
+        encoder: Box::new(encoder()),
+        hasher: Box::new(hasher()),
+    };
+    let merged_hash = cmd.execute(&mut store, &mut refs).unwrap();
+    let obj = store.get(&merged_hash).unwrap().unwrap();
+    let tree = match obj {
+        Object::Tree(t) => t,
         _ => panic!("expected tree"),
     };
 
-    let mut entries = merged_tree.into_entries();
+    let mut entries = tree.into_entries();
     entries.sort_by(|a, b| a.name.cmp(&b.name));
     assert_eq!(entries.len(), 3);
-    assert_eq!(entries[0].name, "a");
     assert_eq!(entries[0].hash, a1);
-    assert_eq!(entries[1].name, "b");
     assert_eq!(entries[1].hash, b3);
-    assert_eq!(entries[2].name, "c");
     assert_eq!(entries[2].hash, c4);
 }
 
 #[test]
 fn merge_conflict_blob() {
     let mut store = setup_store();
-
-    let base_data = store
-        .put(&Object::Blob(Blob::new(b"base".to_vec())))
-        .unwrap();
-    let ours_data = store
-        .put(&Object::Blob(Blob::new(b"ours".to_vec())))
-        .unwrap();
-    let theirs_data = store
-        .put(&Object::Blob(Blob::new(b"theirs".to_vec())))
-        .unwrap();
+    let mut refs = setup_refs();
+    let base_blob = put_blob(&mut store, b"base");
+    let ours_blob = put_blob(&mut store, b"ours");
+    let theirs_blob = put_blob(&mut store, b"theirs");
 
     let base_tree =
-        Tree::new(vec![TreeEntry::new("f".into(), EntryKind::Blob, base_data)]).unwrap();
-    let base_hash = store.put(&Object::Tree(base_tree)).unwrap();
-
+        Tree::new(vec![TreeEntry::new("f".into(), EntryKind::Blob, base_blob)]).unwrap();
+    let base_hash = put_tree(&mut store, &base_tree);
     let ours_tree =
-        Tree::new(vec![TreeEntry::new("f".into(), EntryKind::Blob, ours_data)]).unwrap();
-    let ours_hash = store.put(&Object::Tree(ours_tree)).unwrap();
-
+        Tree::new(vec![TreeEntry::new("f".into(), EntryKind::Blob, ours_blob)]).unwrap();
+    let ours_hash = put_tree(&mut store, &ours_tree);
     let theirs_tree = Tree::new(vec![TreeEntry::new(
         "f".into(),
         EntryKind::Blob,
-        theirs_data,
+        theirs_blob,
     )])
     .unwrap();
-    let theirs_hash = store.put(&Object::Tree(theirs_tree)).unwrap();
+    let theirs_hash = put_tree(&mut store, &theirs_tree);
 
-    let resolver = |_: &[u8], _: &[u8], _: &[u8]| -> Option<Vec<u8>> { None };
-    let err = merge_trees(&mut store, &base_hash, &ours_hash, &theirs_hash, &resolver).unwrap_err();
+    let cmd = MergeCommand {
+        base: base_hash,
+        ours: ours_hash,
+        theirs: theirs_hash,
+        merger: Box::new(ThreeWayMerger),
+        resolver: Box::new(SimpleResolver),
+        encoder: Box::new(encoder()),
+        hasher: Box::new(hasher()),
+    };
+    let err = cmd.execute(&mut store, &mut refs).unwrap_err();
     match err {
         VctrlError::MergeConflict { entry, .. } => assert_eq!(entry, "f"),
         _ => panic!("expected MergeConflict"),
@@ -94,46 +116,45 @@ fn merge_conflict_blob() {
 #[test]
 fn merge_resolved() {
     let mut store = setup_store();
-
-    let base_data = store
-        .put(&Object::Blob(Blob::new(b"base".to_vec())))
-        .unwrap();
-    let ours_data = store
-        .put(&Object::Blob(Blob::new(b"ours".to_vec())))
-        .unwrap();
-    let theirs_data = store
-        .put(&Object::Blob(Blob::new(b"theirs".to_vec())))
-        .unwrap();
+    let mut refs = setup_refs();
+    let base_blob = put_blob(&mut store, b"base");
+    let ours_blob = put_blob(&mut store, b"ours");
+    let theirs_blob = put_blob(&mut store, b"theirs");
 
     let base_tree =
-        Tree::new(vec![TreeEntry::new("f".into(), EntryKind::Blob, base_data)]).unwrap();
-    let base_hash = store.put(&Object::Tree(base_tree)).unwrap();
-
+        Tree::new(vec![TreeEntry::new("f".into(), EntryKind::Blob, base_blob)]).unwrap();
+    let base_hash = put_tree(&mut store, &base_tree);
     let ours_tree =
-        Tree::new(vec![TreeEntry::new("f".into(), EntryKind::Blob, ours_data)]).unwrap();
-    let ours_hash = store.put(&Object::Tree(ours_tree)).unwrap();
-
+        Tree::new(vec![TreeEntry::new("f".into(), EntryKind::Blob, ours_blob)]).unwrap();
+    let ours_hash = put_tree(&mut store, &ours_tree);
     let theirs_tree = Tree::new(vec![TreeEntry::new(
         "f".into(),
         EntryKind::Blob,
-        theirs_data,
+        theirs_blob,
     )])
     .unwrap();
-    let theirs_hash = store.put(&Object::Tree(theirs_tree)).unwrap();
+    let theirs_hash = put_tree(&mut store, &theirs_tree);
 
-    let resolver = |_: &[u8], _: &[u8], _: &[u8]| -> Option<Vec<u8>> { Some(b"resolved".to_vec()) };
-
-    let merged_hash =
-        merge_trees(&mut store, &base_hash, &ours_hash, &theirs_hash, &resolver).unwrap();
-    let merged_tree = match store.get(&merged_hash).unwrap() {
-        Some(Object::Tree(t)) => t,
+    let cmd = MergeCommand {
+        base: base_hash,
+        ours: ours_hash,
+        theirs: theirs_hash,
+        merger: Box::new(ThreeWayMerger),
+        resolver: Box::new(KeepOursResolver),
+        encoder: Box::new(encoder()),
+        hasher: Box::new(hasher()),
+    };
+    let merged_hash = cmd.execute(&mut store, &mut refs).unwrap();
+    let obj = store.get(&merged_hash).unwrap().unwrap();
+    let tree = match obj {
+        Object::Tree(t) => t,
         _ => panic!("expected tree"),
     };
-    let entries = merged_tree.into_entries();
+    let entries = tree.into_entries();
     assert_eq!(entries.len(), 1);
-    let blob = match store.get(&entries[0].hash).unwrap() {
-        Some(Object::Blob(b)) => b,
+    let blob = match store.get(&entries[0].hash).unwrap().unwrap() {
+        Object::Blob(b) => b,
         _ => panic!("expected blob"),
     };
-    assert_eq!(blob.into_bytes(), b"resolved");
+    assert_eq!(blob.into_bytes(), b"ours");
 }
