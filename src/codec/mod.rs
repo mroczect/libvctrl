cbc9ec4eca107634f987bed948dfe6dceeb75ee9
mroczect <mroczect@proton.me>
2026-08-04 19:48:07 +0700

    feat(codec): add module with encoder trait

--- src/codec/mod.rs
+++ src/codec/mod.rs
@@ -1,3 +1,10 @@
 pub mod binary;
-
 pub use binary::*;
+
+use crate::domain::commit::Commit;
+use crate::domain::tree::Tree;
+
+pub trait Encoder {
+    fn encode_tree(&self, tree: &Tree, buf: &mut Vec<u8>);
+    fn encode_commit(&self, commit: &Commit, buf: &mut Vec<u8>);
+}
