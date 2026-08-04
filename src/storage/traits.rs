4ba50d8a3b709b222b7d91e98e47dacf23fd1080
mroczect <mroczect@proton.me>
2026-08-05 00:36:58 +0700

    feat(storage): add ObjectStoreExt for typed getters

--- src/storage/traits.rs
+++ src/storage/traits.rs
@@ -1,5 +1,7 @@
+use crate::domain::commit::Commit;
 use crate::domain::hash::Hash;
 use crate::domain::object::Object;
+use crate::domain::tree::Tree;
 use crate::error::VctrlError;
 
 pub trait ObjectStore {
@@ -16,4 +18,31 @@ pub trait RefStore {
     fn head(&self) -> Result<Option<Hash>, VctrlError>;
     fn head_ref_name(&self) -> Result<Option<String>, VctrlError>;
     fn list_refs(&self, prefix: &str) -> Result<Vec<String>, VctrlError>;
+}
+
+pub trait ObjectStoreExt {
+    fn get_commit(&self, hash: &Hash) -> Result<Commit, VctrlError>;
+    fn get_tree(&self, hash: &Hash) -> Result<Tree, VctrlError>;
+    fn get_blob(&self, hash: &Hash) -> Result<Vec<u8>, VctrlError>;
+}
+
+impl<T: ObjectStore + ?Sized> ObjectStoreExt for T {
+    fn get_commit(&self, hash: &Hash) -> Result<Commit, VctrlError> {
+        match self.get(hash)? {
+            Some(Object::Commit(c)) => Ok(*c),
+            _ => Err(VctrlError::NotFound(format!("commit '{}' not found", hash))),
+        }
+    }
+    fn get_tree(&self, hash: &Hash) -> Result<Tree, VctrlError> {
+        match self.get(hash)? {
+            Some(Object::Tree(t)) => Ok(t),
+            _ => Err(VctrlError::NotFound(format!("tree '{}' not found", hash))),
+        }
+    }
+    fn get_blob(&self, hash: &Hash) -> Result<Vec<u8>, VctrlError> {
+        match self.get(hash)? {
+            Some(Object::Blob(b)) => Ok(b.into_bytes()),
+            _ => Err(VctrlError::NotFound(format!("blob '{}' not found", hash))),
+        }
+    }
 }
