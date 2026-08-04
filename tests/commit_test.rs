7f370557a97a3abf85e43fa9e26ad2700be425b5
mroczect <mroczect@proton.me>
2026-08-04 19:48:15 +0700

    test: update commit test

--- tests/commit_test.rs
+++ tests/commit_test.rs
@@ -1,36 +1,36 @@
 mod common;
-use common::{alice, bob, setup_refs, setup_store};
+use common::{alice, bob, put_blob, put_tree, setup_refs, setup_store};
 
-use libvctrl::{Blob, Commit, Object, ObjectStore, RefStore, Tree, create_commit, get_commit, log};
+use libvctrl::{Command, CreateCommit, EntryKind, Log, Tree, TreeEntry};
 
 #[test]
-fn commit_new_via_create_commit() {
+fn create_commit_and_log() {
     let mut store = setup_store();
     let mut refs = setup_refs();
 
-    let tree = Tree::new(vec![]).unwrap();
-    let tree_hash = store.put(&Object::Tree(tree)).unwrap();
+    let blob_hash = put_blob(&mut store, b"data");
+    let entry = TreeEntry::new("file.txt".into(), EntryKind::Blob, blob_hash);
+    let tree = Tree::new(vec![entry]).unwrap();
+    let tree_hash = put_tree(&mut store, &tree);
 
-    let c1 = create_commit(
-        &mut store,
+    let cmd = CreateCommit {
         tree_hash,
-        vec![],
-        alice(),
-        alice(),
-        "init".into(),
-    )
-    .unwrap();
-    refs.set_ref("refs/heads/main", &c1).unwrap();
-    refs.set_head("refs/heads/main").unwrap();
+        parents: vec![],
+        author: alice(),
+        committer: alice(),
+        message: "initial".into(),
+        encoder: Box::new(common::encoder()),
+        hasher: Box::new(common::hasher()),
+    };
+    let commit_hash = cmd.execute(&mut store, &mut refs).unwrap();
+    assert_eq!(commit_hash.as_bytes().len(), 64);
 
-    let commit = get_commit(&store, &c1).unwrap().unwrap();
-    assert_eq!(commit.message(), "init");
-    assert_eq!(commit.parents().len(), 0);
-    assert_eq!(commit.tree(), &tree_hash);
-
-    let history = log(&store, &refs).unwrap();
+    let log_cmd = Log;
+    let history = log_cmd.execute(&mut store, &mut refs).unwrap();
     assert_eq!(history.len(), 1);
-    assert_eq!(history[0].message(), "init");
+    assert_eq!(history[0].message, "initial");
+    assert_eq!(history[0].tree, tree_hash);
+    assert_eq!(history[0].parents.len(), 0);
 }
 
 #[test]
@@ -38,64 +38,57 @@ fn commit_chain_log() {
     let mut store = setup_store();
     let mut refs = setup_refs();
 
-    let tree = Tree::new(vec![]).unwrap();
-    let tree_hash = store.put(&Object::Tree(tree)).unwrap();
+    let blob_hash = put_blob(&mut store, b"data");
+    let entry = TreeEntry::new("f".into(), EntryKind::Blob, blob_hash);
+    let tree = Tree::new(vec![entry]).unwrap();
+    let tree_hash = put_tree(&mut store, &tree);
 
-    let c1 = create_commit(
-        &mut store,
+    let cmd1 = CreateCommit {
         tree_hash,
-        vec![],
-        alice(),
-        alice(),
-        "first".into(),
-    )
-    .unwrap();
-    refs.set_ref("refs/heads/main", &c1).unwrap();
-    refs.set_head("refs/heads/main").unwrap();
+        parents: vec![],
+        author: alice(),
+        committer: alice(),
+        message: "first".into(),
+        encoder: Box::new(common::encoder()),
+        hasher: Box::new(common::hasher()),
+    };
+    let c1 = cmd1.execute(&mut store, &mut refs).unwrap();
 
-    let c2 = create_commit(
-        &mut store,
+    let cmd2 = CreateCommit {
         tree_hash,
-        vec![c1],
-        bob(),
-        bob(),
-        "second".into(),
-    )
-    .unwrap();
-    refs.set_ref("refs/heads/main", &c2).unwrap();
+        parents: vec![c1],
+        author: bob(),
+        committer: bob(),
+        message: "second".into(),
+        encoder: Box::new(common::encoder()),
+        hasher: Box::new(common::hasher()),
+    };
+    let _c2 = cmd2.execute(&mut store, &mut refs).unwrap();
 
-    let history = log(&store, &refs).unwrap();
+    let log_cmd = Log;
+    let history = log_cmd.execute(&mut store, &mut refs).unwrap();
     assert_eq!(history.len(), 2);
-    assert_eq!(history[0].message(), "second");
-    assert_eq!(history[1].message(), "first");
-}
-
-#[test]
-fn commit_new_validates_hash() {
-    let tree_hash = Blob::new(b"t".to_vec()).hash().unwrap();
-    let commit = Commit::new(tree_hash, vec![], alice(), alice(), "msg".into(), None);
-    assert!(commit.is_ok());
+    assert_eq!(history[0].message, "second");
+    assert_eq!(history[1].message, "first");
 }
 
 #[test]
 fn commit_getters() {
-    let tree_hash = Blob::new(b"t".to_vec()).hash().unwrap();
+    let blob_hash = put_blob(&mut setup_store(), b"t");
     let author = alice();
     let committer = bob();
-    let commit = Commit::new(
-        tree_hash,
+    let commit = libvctrl::Commit::new(
+        blob_hash,
         vec![],
         author.clone(),
         committer.clone(),
         "getter test".into(),
         Some(vec![1, 2, 3]),
-    )
-    .unwrap();
-
-    assert_eq!(commit.tree(), &tree_hash);
-    assert_eq!(commit.message(), "getter test");
-    assert_eq!(commit.author().name, author.name);
-    assert_eq!(commit.committer().email, committer.email);
-    assert_eq!(commit.signature(), Some(&[1, 2, 3][..]));
-    assert!(commit.timestamp().timestamp() > 0);
+    );
+    assert_eq!(commit.tree, blob_hash);
+    assert_eq!(commit.message, "getter test");
+    assert_eq!(commit.author.name, author.name);
+    assert_eq!(commit.committer.email, committer.email);
+    assert_eq!(commit.signature, Some(vec![1, 2, 3]));
+    assert!(commit.timestamp.timestamp() > 0);
 }
