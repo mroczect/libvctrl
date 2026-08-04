7a7516753b9a562540171e281f857626b41fd4f2
mroczect <mroczect@proton.me>
2026-08-04 19:48:15 +0700

    test: update common helpers

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,6 +1,9 @@
 #![allow(dead_code)]
 
-use libvctrl::{MemoryRefStore, MemoryStore, UserInfo};
+use libvctrl::{
+    BinaryEncoder, Encoder, Hash, Hasher, MemoryRefStore, MemoryStore, ObjectStore, Sha512Hasher,
+    UserInfo,
+};
 
 pub fn setup_store() -> MemoryStore {
     MemoryStore::new()
@@ -8,6 +11,14 @@ pub fn setup_store() -> MemoryStore {
 
 pub fn setup_refs() -> MemoryRefStore {
     MemoryRefStore::new()
+}
+
+pub fn encoder() -> BinaryEncoder {
+    BinaryEncoder
+}
+
+pub fn hasher() -> Sha512Hasher {
+    Sha512Hasher
 }
 
 pub fn user(name: &str, email: &str) -> UserInfo {
@@ -20,4 +31,39 @@ pub fn alice() -> UserInfo {
 
 pub fn bob() -> UserInfo {
     user("Bob", "bob@example.com")
+}
+
+pub fn blob_hash(data: &[u8]) -> Hash {
+    hasher().hash_blob(data)
+}
+
+pub fn tree_hash(tree: &libvctrl::Tree) -> Hash {
+    let encoder = encoder();
+    let hasher = hasher();
+    let mut buf = Vec::new();
+    encoder.encode_tree(tree, &mut buf);
+    hasher.hash_tree_encoded(&buf)
+}
+
+pub fn commit_hash(commit: &libvctrl::Commit) -> Hash {
+    let encoder = encoder();
+    let hasher = hasher();
+    let mut buf = Vec::new();
+    encoder.encode_commit(commit, &mut buf);
+    hasher.hash_commit_encoded(&buf)
+}
+
+pub fn put_blob(store: &mut MemoryStore, data: &[u8]) -> Hash {
+    let blob = libvctrl::Blob::new(data.to_vec());
+    let hash = blob_hash(data);
+    store.put(&hash, &libvctrl::Object::Blob(blob)).unwrap();
+    hash
+}
+
+pub fn put_tree(store: &mut MemoryStore, tree: &libvctrl::Tree) -> Hash {
+    let hash = tree_hash(tree);
+    store
+        .put(&hash, &libvctrl::Object::Tree(tree.clone()))
+        .unwrap();
+    hash
 }
