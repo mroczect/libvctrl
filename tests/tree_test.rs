c9f7ea3e5f63f73042a7bea3487149378c63fc44
mroczect <mroczect@proton.me>
2026-08-04 19:48:16 +0700

    test: update tree test

--- tests/tree_test.rs
+++ tests/tree_test.rs
@@ -1,12 +1,15 @@
-use libvctrl::{Blob, EntryKind, Hash, Tree, TreeEntry, TreeError};
+mod common;
+use common::blob_hash;
 
-fn make_entry(name: &str, hash: Hash) -> TreeEntry {
+use libvctrl::{EntryKind, Tree, TreeEntry, TreeError};
+
+fn make_entry(name: &str, hash: libvctrl::Hash) -> TreeEntry {
     TreeEntry::new(name.to_string(), EntryKind::Blob, hash)
 }
 
 #[test]
 fn tree_new_sorts_entries() {
-    let h = Blob::new(b"x".to_vec()).hash().unwrap();
+    let h = blob_hash(b"x");
     let entries = vec![make_entry("c", h), make_entry("a", h), make_entry("b", h)];
     let tree = Tree::new(entries).unwrap();
     let names: Vec<&str> = tree.entries().iter().map(|e| e.name.as_str()).collect();
@@ -15,7 +18,7 @@ fn tree_new_sorts_entries() {
 
 #[test]
 fn tree_duplicate_entries_error() {
-    let h = Blob::new(b"x".to_vec()).hash().unwrap();
+    let h = blob_hash(b"x");
     let entries = vec![make_entry("a", h), make_entry("a", h)];
     let err = Tree::new(entries).unwrap_err();
     match err {
@@ -25,12 +28,14 @@ fn tree_duplicate_entries_error() {
 
 #[test]
 fn tree_hash_deterministic() {
-    let h = Blob::new(b"d".to_vec()).hash().unwrap();
+    let h = blob_hash(b"d");
     let entries1 = vec![make_entry("a", h), make_entry("b", h)];
     let entries2 = vec![make_entry("b", h), make_entry("a", h)];
     let tree1 = Tree::new(entries1).unwrap();
     let tree2 = Tree::new(entries2).unwrap();
-    assert_eq!(tree1.hash().unwrap(), tree2.hash().unwrap());
+    let hash1 = common::tree_hash(&tree1);
+    let hash2 = common::tree_hash(&tree2);
+    assert_eq!(hash1, hash2);
 }
 
 #[test]
@@ -38,12 +43,11 @@ fn tree_empty() {
     let tree = Tree::new(vec![]).unwrap();
     assert!(tree.is_empty());
     assert_eq!(tree.entries().len(), 0);
-    assert!(tree.hash().is_ok());
 }
 
 #[test]
 fn tree_into_entries() {
-    let h = Blob::new(b"e".to_vec()).hash().unwrap();
+    let h = blob_hash(b"e");
     let entries = vec![make_entry("x", h)];
     let tree = Tree::new(entries.clone()).unwrap();
     assert_eq!(tree.into_entries().len(), 1);
