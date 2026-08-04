869bf488adb7d6bd5b9885ccbfedf7894407b520
mroczect <mroczect@proton.me>
2026-08-04 19:48:09 +0700

    feat(domain): add blob

---
+++ src/domain/blob.rs
@@ -0,0 +1,18 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Blob {
+    pub data: Vec<u8>,
+}
+
+impl Blob {
+    pub fn new(data: Vec<u8>) -> Self {
+        Self { data }
+    }
+    pub fn as_bytes(&self) -> &[u8] {
+        &self.data
+    }
+    pub fn into_bytes(self) -> Vec<u8> {
+        self.data
+    }
+}
