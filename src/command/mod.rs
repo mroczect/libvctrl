0fb8b8b9fb089c67a3df3d0f79b75de81efbe121
mroczect <mroczect@proton.me>
2026-08-04 19:48:09 +0700

    feat(command): add command trait and re-exports

--- src/command/mod.rs
+++ src/command/mod.rs
@@ -9,3 +9,15 @@ pub use checkout::*;
 pub use create_commit::*;
 pub use log::*;
 pub use merge::*;
+
+use crate::error::VctrlError;
+use crate::storage::traits::{ObjectStore, RefStore};
+
+pub trait Command {
+    type Output;
+    fn execute(
+        &self,
+        store: &mut dyn ObjectStore,
+        refs: &mut dyn RefStore,
+    ) -> Result<Self::Output, VctrlError>;
+}
