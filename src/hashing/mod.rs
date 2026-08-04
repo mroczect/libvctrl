576317febb5bdd78cd4df8dc63c5f6a44f2463f1
mroczect <mroczect@proton.me>
2026-08-04 19:48:12 +0700

    feat(hashing): add hasher trait and re-exports

--- src/hashing/mod.rs
+++ src/hashing/mod.rs
@@ -1,3 +1,10 @@
 pub mod sha512;
-
 pub use sha512::*;
+
+use crate::domain::hash::Hash;
+
+pub trait Hasher {
+    fn hash_blob(&self, data: &[u8]) -> Hash;
+    fn hash_tree_encoded(&self, data: &[u8]) -> Hash;
+    fn hash_commit_encoded(&self, data: &[u8]) -> Hash;
+}
