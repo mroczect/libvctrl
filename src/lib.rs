22577a665a5817bcac95ce123f88c6cd0b1a7661
mroczect <mroczect@proton.me>
2026-08-04 19:48:02 +0700

    chore: remove old src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,17 @@
-pub mod error;
 pub mod codec;
 pub mod command;
 pub mod diff;
 pub mod domain;
+pub mod error;
 pub mod hashing;
 pub mod merge;
 pub mod storage;
 
-pub use error::*;
 pub use codec::*;
 pub use command::*;
 pub use diff::*;
 pub use domain::*;
+pub use error::VctrlError;
 pub use hashing::*;
 pub use merge::*;
 pub use storage::*;
