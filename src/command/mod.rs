c53eac41a8fe807d3c4d78eed0b85a54509fee45
mroczect <mroczect@proton.me>
2026-08-04 19:33:01 +0700

    refactor(command): add module with submodule re-exports

---
+++ src/command/mod.rs
@@ -0,0 +1,11 @@
+pub mod branch;
+pub mod checkout;
+pub mod create_commit;
+pub mod log;
+pub mod merge;
+
+pub use branch::*;
+pub use checkout::*;
+pub use create_commit::*;
+pub use log::*;
+pub use merge::*;
