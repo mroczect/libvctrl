fd1874827e093fa1011304117059aa4796a41ba4
mroczect <mroczect@proton.me>
2026-08-05 19:32:03 +0700

    feat(core): add blame, diff, stash modules

--- src/command/mod.rs
+++ src/command/mod.rs
@@ -11,14 +11,20 @@ pub mod verify_commit;
 pub use init::*;
 pub mod merge_branch;
 pub use merge_branch::*;
+pub mod blame;
+pub mod diff;
+pub mod stash;
 
+pub use blame::*;
 pub use branch::*;
 pub use checkout::*;
 pub use cherry_pick::*;
 pub use create_commit::*;
+pub use diff::*;
 pub use log::*;
 pub use merge::*;
 pub use revert::*;
+pub use stash::*;
 pub use tag_cmd::*;
 pub use verify_commit::*;
 
