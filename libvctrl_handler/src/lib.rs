d3317b8488da0648f3397bc8c1f7149907d29436
mroczect <mroczect@proton.me>
2026-08-10 01:41:07 +0700

    docs(handler): add clippy deny attribute examples

--- libvctrl_handler/src/lib.rs
+++ libvctrl_handler/src/lib.rs
@@ -1,3 +1,16 @@
+// #![forbid(unsafe_code)]
+// #![deny(
+//     clippy::all,
+//     clippy::pedantic,
+//     clippy::nursery,
+//     clippy::cargo,
+//     missing_docs,
+//     rust_2018_idioms,
+//     unreachable_pub,
+//     unused_crate_dependencies,
+//     unused_qualifications
+// )]
+
 pub mod constants;
 pub mod enums;
 pub mod errors;
