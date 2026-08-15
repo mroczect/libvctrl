4a75b6ebbfa85023c607e2be1165262328f214d0
mroczect <mroczect@proton.me>
2026-08-15 21:40:41 +0700

    docs(codec): add module docs

--- libvctrl_core/src/codec/mod.rs
+++ libvctrl_core/src/codec/mod.rs
@@ -1,7 +1,10 @@
+//! Binary codec module.
+
+/// Binary decoder for Git objects.
 pub mod binary_decoder;
 
+/// Binary encoder for Git objects.
 pub mod binary_encoder;
 
 pub use binary_decoder::BinaryDecoder;
-
 pub use binary_encoder::BinaryEncoder;
