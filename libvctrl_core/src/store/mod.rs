85fb70b47e545590df95ad31d20a227c51e7ac02
mroczect <mroczect@proton.me>
2026-08-15 21:40:42 +0700

    docs(store): add module docs

--- libvctrl_core/src/store/mod.rs
+++ libvctrl_core/src/store/mod.rs
@@ -1,7 +1,10 @@
+//! Store module.
+
+/// In-memory object store.
 pub mod memory;
 
+/// In-memory reference store.
 pub mod ref_store;
 
 pub use memory::MemoryStore;
-
 pub use ref_store::MemoryRefStore;
