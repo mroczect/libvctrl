9f82250de3b87a1622c751ff7b442b222784f53d
mroczect <mroczect@proton.me>
2026-08-16 23:18:21 +0700

    docs(hasher): document trait methods

--- libvctrl_handler/src/traits/core/hasher.rs
+++ libvctrl_handler/src/traits/core/hasher.rs
@@ -2,6 +2,12 @@ use crate::errors::VctrlError;
 use crate::types::Hash;
 use std::io::Read;
 
+/// Trait for computing hash values.
 pub trait Hasher: Send + Sync {
+    /// Returns the hash of the data read from the given reader.
+    ///
+    /// # Errors
+    ///
+    /// Returns [`VctrlError`] if hashing fails.
     fn hash<R: Read + Send>(&self, reader: R) -> Result<Hash, VctrlError>;
 }
