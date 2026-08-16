d9b30ce261377e5b54c59a4af25919334deba4b7
mroczect <mroczect@proton.me>
2026-08-16 23:18:19 +0700

    docs(object): add module docs

--- libvctrl_core/src/object/mod.rs
+++ libvctrl_core/src/object/mod.rs
@@ -1,9 +1,15 @@
+//! Object builders for ergonomic construction of Git objects.
+
+/// Blob builder.
 pub mod blob;
 
+/// Commit builder.
 pub mod commit;
 
+/// Tag builder.
 pub mod tag;
 
+/// Tree builder.
 pub mod tree;
 
 pub use blob::BlobBuilder;
