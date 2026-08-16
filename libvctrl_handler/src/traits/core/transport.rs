b05ecb8bd51048b6485edbd219224424abc66513
mroczect <mroczect@proton.me>
2026-08-16 23:18:22 +0700

    docs(transport): document trait methods

--- libvctrl_handler/src/traits/core/transport.rs
+++ libvctrl_handler/src/traits/core/transport.rs
@@ -2,8 +2,11 @@ use crate::errors::VctrlError;
 use crate::types::Hash;
 use std::io::Read;
 
+/// Trait for transporting Git objects.
 pub trait Transport: Send + Sync {
+    /// Fetches an object by hash, returning a reader.
     fn fetch_object(&self, hash: &Hash) -> Result<Box<dyn Read + Send + '_>, VctrlError>;
 
+    /// Pushes an object to the remote.
     fn push_object(&mut self, hash: &Hash, data: &[u8]) -> Result<(), VctrlError>;
 }
