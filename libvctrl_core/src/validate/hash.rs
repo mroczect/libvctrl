fb7867ca27e42877b104615ac8a23570e7ac6298
mroczect <mroczect@proton.me>
2026-08-16 23:18:19 +0700

    refactor(validate): remove hash validation module

--- libvctrl_core/src/validate/hash.rs
+++
@@ -1,8 +0,0 @@
-use libvctrl_handler::{HASH_LENGTH, VctrlError};
-
-pub const fn validate_hash_bytes(bytes: &[u8]) -> Result<(), VctrlError> {
-    if bytes.len() != HASH_LENGTH {
-        return Err(VctrlError::InvalidHashLength(bytes.len()));
-    }
-    Ok(())
-}
