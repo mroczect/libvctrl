bb81b7c6223a7c12ff51ffac2066161245234904
mroczect <mroczect@proton.me>
2026-08-16 23:18:21 +0700

    docs(object_store): document trait methods

--- libvctrl_handler/src/traits/core/object_store.rs
+++ libvctrl_handler/src/traits/core/object_store.rs
@@ -2,12 +2,17 @@ use crate::errors::VctrlError;
 use crate::types::Hash;
 use std::io::Read;
 
+/// A trait for storing and retrieving Git objects.
 pub trait ObjectStore: Send + Sync {
+    /// Stores an object under the given hash.
     fn put(&mut self, hash: &Hash, data: &[u8]) -> Result<(), VctrlError>;
 
+    /// Retrieves an object by hash, returning a reader.
     fn get(&self, hash: &Hash) -> Result<Box<dyn Read + Send + '_>, VctrlError>;
 
+    /// Deletes an object by hash.
     fn delete(&mut self, hash: &Hash) -> Result<(), VctrlError>;
 
+    /// Checks whether an object exists.
     fn exists(&self, hash: &Hash) -> Result<bool, VctrlError>;
 }
