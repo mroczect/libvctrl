4872c8ccd866dd5118d5172666dd965a4bfee39f
mroczect <mroczect@proton.me>
2026-08-16 23:18:21 +0700

    docs(remote): document trait methods

--- libvctrl_handler/src/traits/core/remote.rs
+++ libvctrl_handler/src/traits/core/remote.rs
@@ -1,13 +1,19 @@
 use crate::errors::VctrlError;
 
+/// Trait for interacting with remote repositories.
 pub trait Remote: Send + Sync {
+    /// The refspec type.
     type RefSpec: Send + Sync;
 
+    /// The remote reference type.
     type RemoteRef: Send + Sync;
 
+    /// Lists references available on the remote.
     fn list_refs(&self) -> Result<Vec<Self::RemoteRef>, VctrlError>;
 
+    /// Fetches objects according to the given refspecs.
     fn fetch(&mut self, refspecs: &[Self::RefSpec]) -> Result<(), VctrlError>;
 
+    /// Pushes objects according to the given refspecs.
     fn push(&mut self, refspecs: &[Self::RefSpec]) -> Result<(), VctrlError>;
 }
