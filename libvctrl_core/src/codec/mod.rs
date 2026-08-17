973ec240bc92873a8d1e72abcc1e5ee7de34f957
mroczect <mroczect@proton.me>
2026-08-17 18:38:31 +0700

    feat(core-codec): export VERSION

--- libvctrl_core/src/codec/mod.rs
+++ libvctrl_core/src/codec/mod.rs
@@ -68,4 +68,4 @@ pub mod binary_decoder;
 pub mod binary_encoder;
 
 pub use binary_decoder::BinaryDecoder;
-pub use binary_encoder::BinaryEncoder;
+pub use binary_encoder::{BinaryEncoder, VERSION};
