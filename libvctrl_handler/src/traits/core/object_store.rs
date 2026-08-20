7f1c5979f6853c80581f5e5866b8c59599eb12d8
mroczect <mroczect@proton.me>
2026-08-20 20:26:53 +0700

    style(handler): reorder imports

--- libvctrl_handler/src/traits/core/object_store.rs
+++ libvctrl_handler/src/traits/core/object_store.rs
@@ -1,13 +1,11 @@
+use std::io::Read;
+
 use crate::errors::VctrlError;
 use crate::types::Hash;
-use std::io::Read;
 
 pub trait ObjectStore: Send + Sync {
     fn put(&mut self, hash: &Hash, data: &[u8]) -> Result<(), VctrlError>;
-
     fn get(&self, hash: &Hash) -> Result<Box<dyn Read + Send + '_>, VctrlError>;
-
     fn delete(&mut self, hash: &Hash) -> Result<(), VctrlError>;
-
     fn exists(&self, hash: &Hash) -> Result<bool, VctrlError>;
 }
