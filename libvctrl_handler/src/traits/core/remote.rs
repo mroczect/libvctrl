39ebab7f1829311a8a3de67567f7b14acab90b42
mroczect <mroczect@proton.me>
2026-08-20 20:26:53 +0700

    style(handler): remove unnecessary blank lines

--- libvctrl_handler/src/traits/core/remote.rs
+++ libvctrl_handler/src/traits/core/remote.rs
@@ -2,12 +2,9 @@ use crate::errors::VctrlError;
 
 pub trait Remote: Send + Sync {
     type RefSpec: Send + Sync;
-
     type RemoteRef: Send + Sync;
 
     fn list_refs(&self) -> Result<Vec<Self::RemoteRef>, VctrlError>;
-
     fn fetch(&mut self, refspecs: &[Self::RefSpec]) -> Result<(), VctrlError>;
-
     fn push(&mut self, refspecs: &[Self::RefSpec]) -> Result<(), VctrlError>;
 }
