e7b14d39f3c2d58cf3f9e1f6435eb590d9f940d8
mroczect <mroczect@proton.me>
2026-08-21 19:34:33 +0700

    test(core): remove inline tests and obsolete integration tests (#338)
    * test(core): remove inline tests from binary_decoder
    
    * test(core): remove inline tests from binary_encoder
    
    * test(core): remove inline tests from sha512
    
    * test(core): remove inline tests from blob
    
    * test(core): remove inline tests from commit
    
    * test(core): remove inline tests from tag
    
    * test(core): remove inline tests from tree
    
    * test(core): remove inline tests from memory store
    
    * test(core): remove inline tests from ref_store
    
    * test(core): remove builder_api integration tests
    
    * test(core): remove codec_roundtrip integration tests
    
    * test(core): remove common test utilities

--- libvctrl_core/src/object/blob.rs
+++ libvctrl_core/src/object/blob.rs
@@ -21,24 +21,3 @@ impl BlobBuilder {
         Blob::new(self.data)
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_build_success_with_data() {
-        let result = BlobBuilder::new().with_data(vec![1, 2, 3, 4]).build();
-        assert!(result.is_ok(), "BlobBuilder should succeed with valid data");
-    }
-
-    #[test]
-    fn test_build_returns_blob_with_correct_data() {
-        let data = vec![0xDE, 0xAD, 0xBE, 0xEF];
-        let blob = BlobBuilder::new()
-            .with_data(data.clone())
-            .build()
-            .expect("build should succeed");
-        assert_eq!(blob.data(), data.as_slice());
-    }
-}
